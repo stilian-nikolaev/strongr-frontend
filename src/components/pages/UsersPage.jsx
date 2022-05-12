899e909460cd1c47acac803362e734314a91cacd
stilian-nikolaev <74594156+stilian-nikolaev@users.noreply.github.com>
2022-05-12 16:47:23 +0300

    logging workouts from api

--- src/components/pages/UsersPage.jsx
+++ src/components/pages/UsersPage.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import axios from 'axios'
-import { Card, Text } from '@mantine/core'
+import { Card, Center, Text } from '@mantine/core'
 
 export default function UsersPage() {
-  // let jsx;
-  // axios.get('http://localhost:5000/api/workouts')
-  //   .then(response => {
-  //     response.data.forEach(workout => {
-  //       jsx += <Card>
-  //         <Text>{workout.title}</Text>
-  //         </Card>
-  //     })
-  //   })
-  //   .catch(err => console.log(err))
+  axios.get('http://localhost:5000/workouts')
+    .then(response => {
+      response.data.forEach(console.log)
+    })
+    .catch(err => console.log(err))
 
+    return <Text>Loading</Text>
 
-  return (
-    <Text>Hello</Text>
-  )
+  
 }
