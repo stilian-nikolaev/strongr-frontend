69c4caf0b9395556a7df449b33377f79cc50110a
unknown <stili_nikolaev@abv.bg>
2022-05-19 01:21:24 +0300

    added dummy workouts to workout page

--- src/components/users/Workout.jsx
+++ src/components/users/Workout.jsx
@@ -1,24 +1,100 @@
-import { ActionIcon, Card, Center, Container, Text } from '@mantine/core'
+import { ActionIcon, Card, Center, Container, List, SimpleGrid, Stack, Text } from '@mantine/core'
+import axios from 'axios';
 import React from 'react'
 import { MdClose } from 'react-icons/md'
+import { useQuery } from 'react-query';
 
-export default function Workout({onBackClick}) {
+async function fetchWorkout(id) {
+    const res = await axios.get(`http://localhost:5000/workouts/${id}`);
+    return res.data;
+}
+
+export default function Workout({ onBackClick, selectedWorkoutId }) {
+    const { data, status } = useQuery(`workout/${selectedWorkoutId}`, () => fetchWorkout(selectedWorkoutId));
+    if (status === 'loading') {
+        return <p>Loading...</p>
+    }
+
+    if (status === 'error') {
+        return <p>Error :(</p>
+    }
     return (
-        <Center>
-            <Container sx={{ display: 'flex', justifyContent: 'space-between', width: '60vw', marginTop: 20 }}>
-                <Card px={30} sx={{ backgroundColor: 'pink' }} shadow="sm">
-                    <Text sx={{ fontSize: '1.5rem' }}>Pull day</Text>
-                    <Text>Exercises: 8</Text>
-                    <Text>Sets: 16</Text>
-                </Card>
-                <ActionIcon onClick={onBackClick} radius={50} sx={{
-                    border: '1px solid black', backgroundColor: 'pink', '&:hover': {
-                        backgroundColor: '#F9BDC5'
-                    }
+        <Container sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: '60vw',
+            marginTop: 20,
+            padding: 0,
+        }}>
+            <Container sx={{ width: '60vw', padding: 0 }}>
+                <Container sx={{
+                    display: 'inline-flex',
+                    paddingLeft: '1vw',
+                    paddingRight: '4vw',
+                    margin: 0,
+                    borderBottom: '2px solid black',
                 }}>
-                    <MdClose size={30} />
-                </ActionIcon>
+                    <Text sx={{ fontSize: '2rem' }}>Pull day</Text>
+                </Container>
+                <SimpleGrid spacing="md" cols={4} sx={{ marginTop: '30px' }}>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text sx={{ borderBottom: '1px solid black', width: '5vw', marginBottom: '5px' }}>
+                            Pullups
+                        </Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text >10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text>Pullups</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text>Pullups</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text>Pullups</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text>Pullups</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text>Pullups</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                    <Card sx={{ backgroundColor: 'pink', borderRadius: '10px', border: '2px solid grey' }}>
+                        <Text>Pullups</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>10 reps with 5 kg</Text>
+                        <Text>8 reps bodyweight</Text>
+                        <Text>8 reps bodyweight</Text>
+                    </Card>
+                </SimpleGrid>
+
             </Container>
-        </Center>
+            <ActionIcon onClick={onBackClick} radius={50} sx={{
+                border: '1px solid black', backgroundColor: 'pink', '&:hover': {
+                    backgroundColor: '#F9BDC5'
+                }
+            }}>
+                <MdClose size={30} />
+            </ActionIcon>
+        </Container>
     )
 }
