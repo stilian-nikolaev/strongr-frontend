7418c3e6a346823e0722b2053c49c46fa741819d
unknown <stili_nikolaev@abv.bg>
2022-05-18 12:13:01 +0300

    redesigned menu

--- src/components/users/layout/Menu.jsx
+++ src/components/users/layout/Menu.jsx
@@ -1,4 +1,4 @@
-import { Image, List, ListItem, Text } from '@mantine/core'
+import { Image, List, ListItem, Stack, Text } from '@mantine/core'
 import dumbbellIcon from '../../../images/dumbbell.png';
 import settingsIcon from '../../../images/settings.png';
 import foodIcon from '../../../images/food.png';
@@ -9,30 +9,34 @@ export default function Menu() {
     const items = [
         {
             label: 'Workouts',
-            icon: dumbbellIcon,
+            selected: true,
         },
         {
             label: 'Meals',
-            icon: foodIcon,
         },
         {
             label: 'Settings',
-            icon: settingsIcon,
         },
 
     ]
 
     return (
-        <List center spacing="xl">
-            {items.map(x =>
-                <ListItem
-                    key={x.label}
-                    sx={{ '&:hover': { cursor: 'pointer' } }}
-                    icon={<Image width={45} src={x.icon}></Image>}>
-                    <Text sx={{ fontSize: '1vw', marginTop: '0.5vh', marginLeft: '0.5vw' }}>
-                        {x.label}
-                    </Text>
-                </ListItem>)}
-        </List>
+        <Stack>
+            {items.map(x => (
+                <Text sx={{
+                    width: '5vw',
+                    color: x.selected ? 'grey' : 'black',
+                    fontSize: x.selected ? '1.1vw' : '1vw',
+                    borderBottom: x.selected ? '2px solid black': '',
+                    marginTop: '0.5vh',
+                    marginLeft: '0.5vw',
+                    '&:hover': {
+                        cursor: 'pointer'
+                    }
+                }}>
+                    {x.label}
+                </Text>
+            ))}
+        </Stack>
     )
 }
