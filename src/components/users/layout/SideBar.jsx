0f80f40dbc0dc0e2b5db5672cf21e7bc275ce0c9
unknown <stili_nikolaev@abv.bg>
2022-05-19 02:56:08 +0300

    redesigned sidebar

--- src/components/users/layout/SideBar.jsx
+++ src/components/users/layout/SideBar.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Avatar, Center, Image, List, ListItem, MenuItem, Navbar, Text, ThemeIcon } from '@mantine/core'
 import logo from '../../../images/logo.jpg'
-import avatar from '../../../images/avatar.jpg'
 import Menu from './Menu'
 
 export default function SideBar() {
     return (
-        <Navbar p="xs" width={{ base: '15vw' }}>
-            <Navbar.Section sx={{ display: 'flex', marginTop: 15}}>
-                <Avatar src={logo} size={60} radius={50} sx={{border: '2px solid black'}} />
-                <Center>
-                    <Text sx={({ marginLeft: '1vw', fontWeight: 'bold', fontSize: '1.5rem' })}>Strongr</Text>
-                </Center>
+        <Navbar px="1vw" py="2vh" width={{ base: '15vw' }}>
+            <Navbar.Section sx={{ display: 'flex', marginTop: 15, '&:hover': { cursor: 'pointer' } }}>
+                <Avatar src={logo} alt="logo" size="4.4vw" radius={50} sx={{
+                    marginLeft: '5px',
+                    border: '2px solid black'
+                }} />
+                <Text sx={({
+                    marginLeft: '1vw',
+                    fontWeight: 'bold',
+                    fontSize: '1.8vw',
+                    alignSelf: 'center'
+                })}>Strongr</Text>
             </Navbar.Section>
             <Navbar.Section grow mt="10vh" ml="sm">
-                <Menu/>
+                <Menu />
             </Navbar.Section>
         </Navbar>
     )
