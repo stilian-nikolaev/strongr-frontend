630754e5b7b6928142f1fe2249137734d0c7be3d
unknown <stili_nikolaev@abv.bg>
2022-05-20 18:23:53 +0300

    added add exercise button

--- src/components/exercises/ExercisesGrid.jsx
+++ src/components/exercises/ExercisesGrid.jsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Card, Center, Box, List, Paper, SimpleGrid, Stack, Text, Container, Loader } from '@mantine/core'
+import { ActionIcon, Card, Center, Box, List, Paper, SimpleGrid, Stack, Text, Container, Loader, Button } from '@mantine/core'
 import { useHover } from '@mantine/hooks';
 import axios from 'axios';
 import React from 'react'
@@ -21,14 +21,14 @@ export default function ExercisesGrid({ setView, selectedWorkoutId }) {
     }
 
     return (
-        <Container sx={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '60vw',
-            marginTop: 10,
-            padding: 0,
-        }}>
-            <Box sx={{ width: '60vw' }}>
+        <Container>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                width: '60vw',
+                marginTop: 10,
+                padding: 0,
+            }}>
                 <Box sx={{
                     display: 'inline-flex',
                     paddingLeft: '1vw',
@@ -38,20 +38,49 @@ export default function ExercisesGrid({ setView, selectedWorkoutId }) {
                 }}>
                     <Text sx={{ fontSize: '2rem' }}>{data.title}</Text>
                 </Box>
-                <SimpleGrid spacing="md" cols={4} sx={{ marginTop: '30px' }}>
-                    
-                    {
+                <Box sx={{ display: 'flex' }}>
+                    <Button  color="dark" size="xs" mr="md">Add exercise</Button>
+                    <ActionIcon onClick={() => setView('workouts')} radius={50}>
+                        <MdClose size={30} />
+                    </ActionIcon>
+                </Box>
+            </Box>
+            <SimpleGrid spacing="md" cols={4} sx={{ marginTop: '30px' }}>
+                {
                     data.exercises.length != 0
                         ?
                         data.exercises.map(x => <ExerciseCard key={x._id} exercise={x} />)
                         :
                         <Text>There are currently no exercises in this workout.</Text>}
-                </SimpleGrid>
-
-            </Box>
-            <ActionIcon onClick={() => setView('workouts')} radius={50}>
-                <MdClose size={30} />
-            </ActionIcon>
+            </SimpleGrid>
         </Container>
+
+        // <Container sx={{
+        //     display: 'flex',
+        //     justifyContent: 'space-between',
+        //     width: '60vw',
+        //     marginTop: 10,
+        //     padding: 0,
+        // }}>
+        //     <Box sx={{ width: '60vw' }}>
+        //         
+        //         <SimpleGrid spacing="md" cols={4} sx={{ marginTop: '30px' }}>
+
+        //             {
+        //                 data.exercises.length != 0
+        //                     ?
+        //                     data.exercises.map(x => <ExerciseCard key={x._id} exercise={x} />)
+        //                     :
+        //                     <Text>There are currently no exercises in this workout.</Text>}
+        //         </SimpleGrid>
+
+        //     </Box>
+        //     <Box sx={{display: 'flex'}}>
+        //         <Button color="dark" size="xs">Add exercise</Button>
+        //         <ActionIcon onClick={() => setView('workouts')} radius={50}>
+        //             <MdClose size={30} />
+        //         </ActionIcon>
+        //     </Box>
+        // </Container>
     )
 }
