cea9d94815bf4883e139e9fbf4ae74678e103138
unknown <stili_nikolaev@abv.bg>
2022-05-21 17:31:01 +0300

    added menu to exercise

--- src/components/exercises/ExerciseCard.jsx
+++ src/components/exercises/ExerciseCard.jsx
@@ -1,8 +1,20 @@
 import { RiMoreFill } from 'react-icons/ri';
-import { Box, Card, Text } from '@mantine/core'
-import React from 'react'
+import { Box, Card, Menu, Text } from '@mantine/core'
+import React, { forwardRef } from 'react'
 
 export default function ExerciseCard({ exercise }) {
+    
+    function onAddSetClick(e) {
+
+    }
+
+    function onEditClick(e) {
+        
+    }
+
+    function onDeleteClick(e) {
+        console.log('delete');
+    }
 
     return (
         <Card shadow="lg" sx={{
@@ -20,18 +32,35 @@ export default function ExerciseCard({ exercise }) {
                 <Text sx={{ borderBottom: '1px solid black', width: '10vw', marginBottom: '5px' }}>
                     {exercise.title}
                 </Text>
-                <Box className="more" sx={{ height: '1vh', padding: 0, display: 'none', '&:hover': { cursor: 'pointer' } }}>
-                    <RiMoreFill size="1.5vw" />
-                </Box>
+                <Menu
+                    control={
+                        <Box className="more" sx={{ display: 'none', '&:hover': { cursor: 'pointer' } }} >
+                            <RiMoreFill size="1.5vw" />
+                        </Box >
+                    }
+                    size="xs"
+                    gutter={-25}
+                >
+                    <Menu.Item onClick={onAddSetClick}>
+                        Add set
+                    </Menu.Item>
+                    <Menu.Item onClick={onEditClick}>
+                        Edit
+                    </Menu.Item>
+                    <Menu.Item onClick={onDeleteClick}>
+                        Delete
+                    </Menu.Item>
+                </Menu>
+
             </Box>
             {exercise.sets.length != 0
-            ?
-            exercise.sets.map(x => 
-            <Text key={x._id}>
-                {x.amount} reps {x.weight > 0 ? `with ${x.weight} kg` : 'bodyweight'}
-            </Text>)
-            :
-            <Text>No sets yet.</Text>}
+                ?
+                exercise.sets.map(x =>
+                    <Text key={x._id}>
+                        {x.amount} reps {x.weight > 0 ? `with ${x.weight} kg` : 'bodyweight'}
+                    </Text>)
+                :
+                <Text>No sets yet.</Text>}
         </Card>
     )
 }
