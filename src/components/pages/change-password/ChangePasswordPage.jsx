72d93029b51b4f1e6af21fd215bb460235b6a5e4
unknown <stili_nikolaev@abv.bg>
2022-06-29 19:33:02 +0300

    simplified page components

--- src/components/pages/change-password/ChangePasswordPage.jsx
+++ src/components/pages/change-password/ChangePasswordPage.jsx
@@ -1,8 +1,85 @@
+import { Box, Center, Text } from '@mantine/core'
 import React from 'react'
-import ChangePasswordForm from './ChangePasswordForm'
+import { useMutation } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+import { useChangePassword } from '../../../hooks/auth'
+import GenericButton from '../../common/buttons/GenericButton'
+import GenericForm from '../../common/form/GenericForm'
+import TextField from '../../common/form/TextField'
+import { showNotification } from '@mantine/notifications';
+import * as yup from 'yup'
 
 export default function ChangePasswordPage() {
-  return (
-    <ChangePasswordForm />
-  )
+    const navigate = useNavigate();
+
+    const validationSchema = yup
+        .object({
+            password: yup.string().trim().min(1).max(40).required(),
+            repeatPassword: yup.string().trim().min(1).max(40).required(),
+        })
+
+    const mutation = useMutation({
+        mutationFn: data => useChangePassword(data),
+        onSuccess: (res) => {
+            navigate('/settings')
+            showNotification({
+                title: 'Success',
+                message: 'Successfully changed password!'
+            })
+        }
+    })
+
+    function onSubmit(data) {
+        mutation.mutate(data)
+    }
+
+    return (
+        <Center sx={{ height: '89vh' }}>
+            <GenericForm validationSchema={validationSchema} initialValues={{ password: '', repeatPassword: '' }} onSubmit={onSubmit}>
+                <Text sx={{ fontSize: '22px', marginLeft: 20 }}>Change password</Text>
+                <TextField
+                    placeholder="New Password"
+                    aria-label="newPassword"
+                    name="password"
+                    size="lg"
+                    type="password"
+                    required
+                    sx={(theme) => ({
+                        marginTop: '20px',
+                        '& ::placeholder': {
+                            color: `${theme.colors.common[1]} !important`
+
+                        }
+                    })}
+                />
+                <TextField
+                    placeholder="Confirm Password"
+                    aria-label="confirmPassword"
+                    name="repeatPassword"
+                    size="lg"
+                    type="password"
+                    required
+                    sx={(theme) => ({
+
+                        marginTop: '40px',
+                        '& ::placeholder': {
+                            color: `${theme.colors.common[1]} !important`
+
+                        }
+                    })}
+                />
+                <GenericButton
+                    px='20px'
+                    py='12px'
+                    type="submit"
+                    sx={{
+                        fontSize: '16px',
+                        marginTop: 30
+                    }}>
+                    Submit
+                </GenericButton>
+
+            </GenericForm>
+        </Center>
+    )
 }
--- src/components/pages/register/RegisterPage.jsx
+++ src/components/pages/register/RegisterPage.jsx
@@ -1,8 +1,146 @@
+import { Box, Center, Text } from '@mantine/core';
 import React from 'react'
-import RegisterForm from './RegisterForm'
+import GenericForm from '../../common/form/GenericForm';
+import TextField from '../../common/form/TextField';
+import { ViewStore } from '../../../stores/ViewStore';
+import { useMutation } from 'react-query';
+import { useConfigureHeaders, useRegisterUser } from '../../../hooks/auth';
+import { AuthStore } from '../../../stores/AuthStore';
+import { useNavigate } from 'react-router-dom';
+import GenericButton from '../../common/buttons/GenericButton';
+import * as yup from 'yup'
 
 export default function RegisterPage() {
-  return (
-    <RegisterForm/>
-  )
+    const navigate = useNavigate();
+    const { login } = AuthStore
+
+
+    const validationSchema = yup
+        .object({
+            email: yup.string().email().required(),
+            name: yup.string().min(1).max(40).required(),
+            password: yup.string().trim().min(1).max(40).required(),
+            repeatPassword: yup.string().trim().min(1).max(40).required(),
+        })
+
+    const mutation = useMutation({
+        mutationFn: data => useRegisterUser(data),
+        onSuccess: (res) => {
+            useConfigureHeaders(res.token);
+            login(res.token, res.expiresAt)
+            navigate('/workouts')
+        }
+    })
+
+
+
+    function onSubmit(data) {
+        mutation.mutate(data)
+    }
+
+    function onLogInClick() {
+        navigate('/login')
+    }
+
+    return (
+        <Center sx={{ height: '35vw', marginTop: '2vw' }}>
+            <GenericForm
+                validationSchema={validationSchema}
+                initialValues={{ email: '', password: '', repeatPassword: '', name: '' }}
+                onSubmit={onSubmit}>
+                <Box sx={{ width: 320 }}>
+                    <Text sx={{ fontSize: '22px', textAlign: 'center' }}>Create an account</Text>
+                    <TextField
+                        placeholder="Name"
+                        aria-label="name"
+                        name="name"
+                        size="lg"
+                        required
+                        sx={(theme) => ({
+                            marginTop: '20px',
+                            '& ::placeholder': {
+                                color: `${theme.colors.common[1]} !important`
+
+                            }
+                        })}
+                    />
+                    <TextField
+                        placeholder="Email"
+                        aria-label="email"
+                        name="email"
+                        size="lg"
+                        required
+                            sx={(theme) => ({
+                            marginTop: '40px',
+                            '& ::placeholder': {
+                                color: `${theme.colors.common[1]} !important`
+
+
+                            }
+                        })}
+                    />
+                    <TextField
+                        placeholder="Password"
+                        aria-label="password"
+                        name="password"
+                        size="lg"
+                        type="password"
+                        required
+                            sx={(theme) => ({
+                            marginTop: '40px',
+                            '& ::placeholder': {
+                                color: `${theme.colors.common[1]} !important`
+
+                            }
+                        })}
+                    />
+                    <TextField
+                        placeholder="Confirm Password"
+                        aria-label="confirmPassword"
+                        name="repeatPassword"
+                        size="lg"
+                        type="password"
+                        required
+                        sx={(theme) => ({
+                            marginTop: '40px',
+                            '& ::placeholder': {
+                                color: `${theme.colors.common[1]} !important`
+
+                            }
+                        })}
+                    />
+                    <Box sx={{ display: 'flex', marginTop: 40 }}>
+                        <GenericButton
+                            px='20px'
+                            py='12px'
+                            type="submit"
+                            sx={{
+                                color: 'white',
+                                fontSize: '16px',
+                            }}>
+                            Sign up
+                        </GenericButton>
+                        <Text sx={{ marginLeft: 20 }}>
+                            Already have an account?
+                            <Text
+                                onClick={onLogInClick}
+                                sx={(theme) => ({
+                                    backgroundImage: `linear-gradient(${theme.colors.common[0]}, ${theme.colors.common[0]})`,
+                                    width: '100px',
+                                    backgroundSize: '0% 2px',
+                                    backgroundRepeat: 'no-repeat',
+                                    transition: 'background-size 0.2s',
+                                    backgroundPosition: '50% calc(100%)',
+                                    '&:hover': {
+                                        backgroundSize: '100% 2px',
+                                        cursor: 'pointer'
+                                    }
+                                })}>Log in here</Text>
+                        </Text>
+                    </Box>
+                </Box>
+            </GenericForm>
+        </Center>
+
+    )
 }
